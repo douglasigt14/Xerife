6c2b268fb743dd2e6773ae33c3c8bd5ac455b717
Douglas Rocha <douglas.rocha@americanas.delivery>
2023-06-06 15:02:46 -0300

    WIP

--- src/app/components/calendar/calendar.component.ts
+++ src/app/components/calendar/calendar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterContentInit, AfterViewInit, Component, OnInit } from '@angular/core';
 import { CalendarOptions } from '@fullcalendar/core'; 
 import ptBrLocale from '@fullcalendar/core/locales/pt-br'; // useful for typechecking
 import timeGridPlugin from '@fullcalendar/timegrid'
@@ -8,18 +8,38 @@ import timeGridPlugin from '@fullcalendar/timegrid'
   templateUrl: './calendar.component.html',
   styleUrls: ['./calendar.component.scss']
 })
-export class CalendarComponent implements OnInit{
-
+export class CalendarComponent implements OnInit, AfterViewInit{
+  events = [{
+    title: 'Douglas | Cabelo e Barba',
+    start: '2023-06-06 10:00:00',
+    end: '2023-06-06 10:30:00',
+    allDay: false,
+    backgroundColor: 'black'
+  }];
   calendarOptions: CalendarOptions = {
     plugins: [timeGridPlugin],
     initialView: 'timeGridWeek',
-    height: '600px',
+    height: '700px',
     locale: ptBrLocale,
+    events: [],
     allDaySlot: false
   };
 
   ngOnInit(): void {
+    this.events.push({
+      title: 'Cliedy | Sombrancelha',
+      start: '2023-06-06 12:00:00',
+      end: '2023-06-06 13:30:00',
+      allDay: false,
+      backgroundColor: 'black'
+    });
+    this.calendarOptions.events = this.events;
+  }
+
+  ngAfterViewInit(): void {
     
   }
+
+
 
 }
