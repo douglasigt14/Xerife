3913dc4de019ed49eb18e9b02ebd77c11431b045
Douglas Rocha <douglas.rocha@americanas.delivery>
2023-06-28 21:55:16 -0300

    wip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { map } from 'rxjs';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'xerife';
   is_auth:boolean = true; 
   cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
@@ -18,5 +19,12 @@ export class AppComponent {
     })
   );
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  ngOnInit(): void {
+    this.authService.getIsAuth().subscribe(value => {
+      this.is_auth = value;
+    })
+  }
+  
+
+  constructor(private breakpointObserver: BreakpointObserver,private authService: AuthService) {}
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,16 +1,26 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   private apiUrl = 'http://localhost:8000/api'; // URL da API do backend
+  public isAuth = new BehaviorSubject<any>(false);
 
   constructor(private http: HttpClient) { }
 
+  setIsAuth(value:boolean){
+    this.isAuth.next(value)
+  }
+
+  getIsAuth(){
+    return this.isAuth
+  }
+
   login(email: string, password: string): Observable<any> {
+    this.setIsAuth(true);
     return this.http.post<any>(`${this.apiUrl}/login`, { email, password });
   }
 
@@ -19,7 +29,7 @@ export class AuthService {
   }
 
   logout(): void {
-    // Limpe o token JWT do armazenamento (por exemplo, localStorage)
+    this.setIsAuth(false);
     localStorage.removeItem('token');
   }
 }
